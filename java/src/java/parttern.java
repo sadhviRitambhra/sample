017524e44492bf34dac3faf833d7ae2c711f8e76
Swayam <Swayam>
2018-03-21 15:39:26 +0530

    First Commit of sample Project

---
+++ java/src/java/parttern.java
@@ -0,0 +1,16 @@
+package java;
+
+public class parttern {
+	public static void main(String[] args)
+	{
+		int x=9;
+		for(x=5;x>=1;x--)
+		{
+			for(x=9;x>4;x--) {
+				System.out.println((x*x)-1+"\t");
+		}
+		
+		}
+			
+	}
+}
